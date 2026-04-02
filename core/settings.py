de3837fd673d834b1f43bdbc5856c57784451023
Srishti Agarwal <114217512+srishti-cmd@users.noreply.github.com>
2026-04-02 13:57:08 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-5lz8a48mgzby18)kjtl67lr0tj7*qk04^qbgmk@5fx4fgfj)qi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
